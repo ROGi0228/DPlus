e0ccabd324c97a8a2a342535bb1b66d25740bba0
ROGi0228 <84007090+ROGi0228@users.noreply.github.com>
2024-03-11 12:05:27 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,3 +38,17 @@ const swiper = new Swiper('.swiper', {
       menuBtn.classList.toggle('active');
       menu.classList.toggle('active');
     })
+
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.catalog_card').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__fadeInUp");
+	        }
+	    });
+	});
+})
